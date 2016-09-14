4339b937c100b20d8b678b153aedd6603fde07d0
deviant <lubotin@gmail.com>
2016-09-14 23:52:21 +0300

    gitignore setup

--- library/src/main/java/ds/salo/BindingFragment.kt
+++ library/src/main/java/ds/salo/BindingFragment.kt
@@ -5,7 +5,7 @@ import android.databinding.ViewDataBinding
 import android.support.v4.app.Fragment
 
 abstract class BindingFragment : Fragment(), BindingAware {
-    
+
     override val delegate: BinderDelegate = BinderDelegate()
     override val binding: ViewDataBinding by lazy { delegate.binding!! }
     override val activity: Activity by lazy { getActivity() }
